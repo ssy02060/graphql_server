ba949a07f05cc3e0ee0120606366f18eacd2872d
Seo <ssy02060@gmail.com>
2020-05-10 07:09:24 +0900

    왜이러징...

---
+++ graphql/db.js
@@ -0,0 +1,25 @@
+export const books = [
+    {
+        bookId : 0,
+        bookName : "나미야 잡화점",
+        publishier : "출판사",
+        author : "히가시노 게이고"
+    },
+    {
+        bookId : 1,
+        bookName : "불안",
+        publishier : "출판사",
+        author : "알랭드 보통"
+    },
+    {
+        bookId : 2,
+        bookName : "빅픽쳐",
+        publishier : "출판사",
+        author : "더글라스 케네디"
+    }
+]
+export const bookstore = {
+    storeId: 0,
+    storeName: "교보문고",
+    location: "동성로"
+}
--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,19 +1,10 @@
 //resolver를 어떻게 정의하는지에 따라 다른 DB에 접근하거나 할 수 있다.
+import {DB} from "./db"
 
-const book = {
-    bookId : 0,
-    bookName : "나미야 잡화점",
-    publishier : "출판사",
-    author : "히가시노 게이고"
-}
-const bookstore = {
-    storeId: 0,
-    storeName: "교보문고",
-    location: "동성로"
-}
+
 const resolvers = {
     Query :{
-        Books : () => book,
+        Books : () => books,
         Bookstores : () => bookstore
     }
 } 
