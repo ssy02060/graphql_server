7c28b0896c42982411bd686f0fc242f9396b86ba
Seo <ssy02060@gmail.com>
2020-05-12 11:29:47 +0900

    getBookById, books 쿼리 구현

--- graphql/db.js
+++ graphql/db.js
@@ -1,25 +1,37 @@
+//export는 import 와 쌍으로 동작한다.
+//다른 모듈(파일에서) 사용할 수 있게 보내주는 느낌이다.
 export const books = [
     {
-        bookId : 0,
-        bookName : "나미야 잡화점",
+        id : 0,
+        name : "나미야 잡화점",
         publishier : "출판사",
         author : "히가시노 게이고"
     },
     {
-        bookId : 1,
-        bookName : "불안",
+        id : 1,
+        name : "불안",
         publishier : "출판사",
         author : "알랭드 보통"
     },
     {
-        bookId : 2,
-        bookName : "빅픽쳐",
+        id : 2,
+        name : "빅픽쳐",
         publishier : "출판사",
         author : "더글라스 케네디"
     }
-]
+];
 export const bookstore = {
-    storeId: 0,
-    storeName: "교보문고",
+    id: 0,
+    name: "교보문고",
     location: "동성로"
+}
+// 함수 표현식 + arrow 함수
+// 함수 표현식은 함수의 참조값을 변수에 저장하는 형식. 
+export const getBookById = id => {
+
+    console.log(id);
+    const filteredBook = books.filter(book => book.id === id);
+    
+    console.log(filteredBook);
+    return filteredBook[0];
 }
--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,12 +1,17 @@
 //resolver를 어떻게 정의하는지에 따라 다른 DB에 접근하거나 할 수 있다.
-import {DB} from "./db"
+import {books, bookstore, getBookById} from "./db";
 
-
+// 키 : 값
+// 변수 : value
+// 객체 안에서 { 변수 : 함수 => 메서드 }
+// 객체 안에 정의된 함수는 메서드라고 정의함.
 const resolvers = {
     Query :{
-        Books : () => books,
-        Bookstores : () => bookstore
+        books : () => books,
+        //_ : object, id: 매개변수
+        book : (_, { id }) =>  getBookById(id),
+        bookstores : () => bookstore
     }
-} 
+};
 
 export default resolvers;
